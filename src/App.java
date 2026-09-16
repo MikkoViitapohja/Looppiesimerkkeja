3c4fc12fd053371fcb8378db81ef80342b332400
MikkoViitapohja <mikkotapioviitapohja@gmail.com>
2026-09-16 22:15:51 +0300

    Printtaus yhdestä viiteen järkevämmin

--- src/App.java
+++ src/App.java
@@ -16,11 +16,18 @@ public class App {
 
         //Print numbers from 1 to 5
 
-        System.out.println("1");
-        System.out.println("2");
-        System.out.println("3");
-        System.out.println("4");
-        System.out.println("5");
+        // System.out.println("1");
+        // System.out.println("2");
+        // System.out.println("3");
+        // System.out.println("4");
+        // System.out.println("5");
+
+        // for (reset counter; condition; increment)
+
+        for (int counter = 0 ; counter < 5; counter++)
+        {
+            System.out.println(counter + 1);
+        }
 
     }
 }
